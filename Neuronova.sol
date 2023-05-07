efef406379b00c37eb2333d51cb6b717f35f411a
HocariFounder <contact@hocari.com>
2023-05-07 18:13:48 -0400

    Update Neuronova.sol

--- Neuronova.sol
+++ Neuronova.sol
@@ -4,21 +4,46 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+contract NeuronovaDeployer {
+    Neuronova public token;
+
+    constructor(uint256 _maxTotalSupply, uint256 _annualBurnRate) {
+        token = new Neuronova(_maxTotalSupply, _annualBurnRate);
+    }
+}
+
 
 contract Neuronova is ERC20Upgradeable, OwnableUpgradeable {
+    using SafeMath for uint256;
+
     uint256 public immutable maxTotalSupply;
     uint256 public immutable annualBurnRate;
     uint256 public lastMintTimestamp;
+    address[] private _allHolders;
+    mapping(address => uint256) private _balances;
     uint256 public constant YEAR_IN_SECONDS = 31536000; // seconds in a year
     
 
-    constructor(uint256 _maxTotalSupply, uint256 _annualBurnRate) ERC20Upgradeable() {
+    constructor(uint256 _maxTotalSupply, uint256 _annualBurnRate) ERC20Upgradeable() OwnableUpgradeable() {
         maxTotalSupply = _maxTotalSupply;
         annualBurnRate = _annualBurnRate;
         lastMintTimestamp = block.timestamp;
+
+        // Set the specified address as the owner
+        _transferOwnership(0xAfB9803dd2aA7EBF5979fc34628775daE0fBF280);
+
+        // Mint 10 million tokens to the contract owner
+        uint256 mintAmount = 10000000 * 10 ** decimals();
+        require(totalSupply() + mintAmount <= maxTotalSupply, "Max total supply exceeded");
+        _mint(owner(), mintAmount);
     }
 
-    function mint() external onlyOwner {
+
+
+    function mint() external {
         uint256 elapsedTime = block.timestamp - lastMintTimestamp; // time since last mint
         if (elapsedTime == 0) {
             return; // don't mint if less than a second has passed
@@ -31,31 +56,59 @@ contract Neuronova is ERC20Upgradeable, OwnableUpgradeable {
         _mint(owner(), mintAmount); // mint the tokens to the owner
     }
 
-    function burn(uint256 amount) external {
-        uint256 senderBalance = balanceOf(msg.sender);
+    function burn() external {
         uint256 totalSupplyBeforeBurn = totalSupply();
+        uint256 totalBurned = 0;
 
         // Check if a year has passed since the last burn
         require(block.timestamp >= lastMintTimestamp + YEAR_IN_SECONDS, "A year has not passed since the last burn");
 
-        // Calculate 5% of the balance of the sender's address at the time of the last burn
-        uint256 burnAmount = senderBalance * 5 / 100;
+        // Calculate the total amount of tokens to be burned, which is a percentage of the total supply
+        uint256 burnPercentage = 5;
+        uint256 totalToBurn = totalSupplyBeforeBurn * burnPercentage / 100;
+
+        // Loop through all token holders and burn a percentage of their balance
+        for (uint i = 0; i < _allHolders.length; i++) {
+            address holder = _allHolders[i];
+            uint256 holderBalance = balanceOf(holder);
+            uint256 holderBurnAmount = holderBalance * burnPercentage / 100;
+            if (holderBurnAmount > 0) {
+                _burn(holder, holderBurnAmount);
+                totalBurned += holderBurnAmount;
+            }
+        }
+
+        // Make sure the total amount burned is not greater than the total to be burned
+        require(totalBurned == totalToBurn, "Total burned amount does not match expected amount");
 
         // Lock the amount of tokens to be burned until the next burn date
         lastMintTimestamp = block.timestamp;
 
-        // Burn the calculated amount of tokens
-        require(amount <= burnAmount, "Burn amount exceeds 5% of the sender's balance at the last burn");
-        require(totalSupplyBeforeBurn - amount >= maxTotalSupply - maxTotalSupply * annualBurnRate / 100, "Max total supply exceeded");
-        _burn(msg.sender, amount);
+        // Make sure the total supply is within the maximum allowed
+        require(totalSupplyBeforeBurn - totalToBurn >= maxTotalSupply - maxTotalSupply * annualBurnRate / 100, "Max total supply exceeded");
     }
+
 
 
     // Override transfer and transferFrom functions to include the _beforeTokenTransfer hook
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        _beforeTokenTransfer(msg.sender, recipient, amount);
-        return super.transfer(recipient, amount);
+    function transfer(address to, uint256 value) public override returns (bool) {
+        require(to != address(0), "Transfer to the zero address");
+        require(value <= _balances[msg.sender], "Insufficient balance");
+        
+        _balances[msg.sender] = _balances[msg.sender].sub(value);
+        _balances[to] = _balances[to].add(value);
+
+        if (!contains(_allHolders, msg.sender)) {
+            _allHolders.push(msg.sender);
+        }
+        if (!contains(_allHolders, to)) {
+            _allHolders.push(to);
+        }
+
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
+
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(sender, recipient, amount);
@@ -109,5 +162,15 @@ contract Neuronova is ERC20Upgradeable, OwnableUpgradeable {
     function symbol() public pure override returns (string memory) {
         return "NIT";
     }
+
+    function contains(address[] memory arr, address addr) private pure returns (bool) {
+        for (uint i = 0; i < arr.length; i++) {
+            if (arr[i] == addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
